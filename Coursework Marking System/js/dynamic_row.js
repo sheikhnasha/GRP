32747ce1af7966042faec6977acaa4b492ec4bb2
tgrazins <tgrazins@yahoo.com>
2015-02-11 12:39:05 +0800

    first draft of the Coursework

---
+++ Coursework Marking System/js/dynamic_row.js
@@ -0,0 +1,16 @@
+     $(document).ready(function(){
+      var i=1;
+     $("#add_row").click(function(){
+      $('#addr'+i).html("<td>"+ (i+1) +"</td><td><input name='name"+i+"' type='text' placeholder='Insert Text' class='form-control input-md'/></td>");
+
+      $('#tab_logic').append('<tr id="addr'+(i+1)+'"></tr>');
+      i++; 
+  });
+     $("#delete_row").click(function(){
+    	 if(i>1){
+		 $("#addr"+(i-1)).html('');
+		 i--;
+		 }
+	 });
+
+});
